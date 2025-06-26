5e49353b0c372eb8bb491473a937f148c5bc618d
Atharvsurya <atharvsurya007@gmail.com>
2025-06-26 12:41:03 +0530

    Test commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let scoreself=0;
+let scorecomp=0;
+const choices = document.querySelectorAll(".images img");
+let lastCompChoice = null;
+
+function getSmartRandomChoice() {
+  const options = ["rock", "paper", "scissors"];
+  let choice;
+  do {
+    comp = options[Math.floor(Math.random() * 3)];
+  } while (comp === lastCompChoice);
+  lastCompChoice = comp;
+  return comp;
+}
+const play =(self)=>{
+    comp=getSmartRandomChoice();
+    if(self===comp){
+        document.getElementById("result").innerHTML = "It was a draw";
+        document.getElementById("result").style.backgroundColor = "black";
+    }
+    else if((self === "rock" && comp === "scissors") ||
+    (self === "paper" && comp === "rock") ||
+    (self === "scissors" && comp === "paper")){
+        scoreself++;
+        document.getElementById("result").innerHTML = "You Win!!";
+        document.getElementById("selfscore").innerHTML =scoreself;
+        document.getElementById("result").style.backgroundColor = "green";
+    }
+    else{
+        scorecomp++;
+        document.getElementById("result").innerHTML = "You lose!! "+comp+" beats your "+self;
+        document.getElementById("compscore").innerHTML =scorecomp;
+        document.getElementById("result").style.backgroundColor = "red";
+    }
+};
+
+choices.forEach((element) => {
+      element.addEventListener("click", () => {
+        const userChoice = element.getAttribute("id");
+        play(userChoice);
+      });
+    });
